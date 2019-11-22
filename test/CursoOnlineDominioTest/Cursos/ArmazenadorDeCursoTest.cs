befe34b32554da8c0e5f2d2ad8431135df9863f4
Marcos Paixao <marcosv2912@gmail.com>
2019-11-22 08:02:27 -0300

    15 a 17

---
+++ test/CursoOnlineDominioTest/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,64 @@
+﻿using CursoOnline.Dominio.Cursos;
+using Moq;
+using System;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        [Fact]
+        public void DeveAdicionarCurso()
+        {
+            var cursoDTO = new CursoDTO
+            {
+                Nome = "Curso A",
+                CargaHoraria = 80,
+                PublicoAlvoId = 1,
+                Valor = 850.00,
+                Descricao = "Descrição"
+            };
+
+            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+
+            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositorioMock.Object);
+
+            armazenadorDeCurso.Armazenar(cursoDTO);
+
+            cursoRepositorioMock.Verify(r=> r.Adicionar(It.IsAny<Curso>()));
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        void Adicionar(Curso curso);
+    }
+
+    public class ArmazenadorDeCurso
+    {
+        private readonly ICursoRepositorio _cursoRepositorio;
+        
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public void Armazenar(CursoDTO cursoDTO)
+        {
+            var curso = new Curso(cursoDTO.Nome, cursoDTO.Descricao,
+                cursoDTO.CargaHoraria, PublicoAlvo.Estudante,
+                cursoDTO.Valor);
+
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+
+    public class CursoDTO
+    {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public int PublicoAlvoId { get; set; }
+        public double Valor { get; set; }
+        public string Descricao { get; set; }
+    }
+}
